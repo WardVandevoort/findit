10d9857c6543115ecb69afa0d833436d0368fdf7
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-10-09 10:57:58 +0200

    added routes

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/student_profile', function () {
+    return view('student_profile');
+});
+
+Route::get('/company_profile', function () {
+    return view('company_profile');
+});
+
+Route::get('/internship', function () {
+    return view('internship');
+});
+
+Route::get('/student', function () {
+    return view('student');
+});
+
+Route::get('/company', function () {
+    return view('company');
+});

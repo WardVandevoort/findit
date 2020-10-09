8bcd2492f47bc65bddf881c3b680dbc27f9296eb
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-10-09 17:06:23 +0200

    Added migrations and seeders

---
+++ project/database/migrations/2020_10_09_124208_create_internships_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInternshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('internships', function (Blueprint $table) {
+            $table->id();
+            $table->string('function'); //PHP-developer etc.
+            $table->text('job_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('internships');
+    }
+}

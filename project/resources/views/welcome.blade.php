bab2c39d2c78fb216ef200666f9d1d8d612cc595
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-10-23 11:36:54 +0200

    deleted views + layout home

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts/app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>hoi</h1>
-</body>
-
-</html>
+@section('content')
+<form class="form-group" action="">
+    <input class="form-control" type="search" placeholder="zoek stageplaats">
+    <button class="btn btn-primary" type="submit">Zoek</button>
+</form>
+@endsection

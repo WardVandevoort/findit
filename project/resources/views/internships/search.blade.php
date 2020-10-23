cf0179fe99901e10e2b183a250e1f4065f2d2665
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-10-23 14:30:31 +0200

    added link to internship page

--- project/resources/views/internships/search.blade.php
+++ project/resources/views/internships/search.blade.php
@@ -7,7 +7,7 @@
     <h1>zoek resultaten</h1>
     <p> resultaten voor '{{request()->input('query')}}'</p>
     @foreach($internships as $internship)
-    <p>{{$internship->title}}</p>
+    <h3><a href="/internships/{{ $internship->id }}">{{ $internship->title }}</a></h3>
     @endforeach
 </div>
 @endsection

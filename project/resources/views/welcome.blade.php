5ff0a05cc468f6dfdf2dbbbe5d4e155f45d2f46c
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-10-26 17:21:07 +0100

    Update welcome.blade.php

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -1,39 +1,12 @@
-<<<<<<< HEAD
-@extends('layouts/indexLayout')
-||||||| d10cfb6
-<!DOCTYPE html>
-<html lang="en">
-=======
 @extends('layouts/app')
 @extends('layouts/homeLayout')
->>>>>>> 7994d7e4daf7fda6d4ba6495f102d43381f321ca
 
-<<<<<<< HEAD
-@section('title')
-Home
-@endsection
-||||||| d10cfb6
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-=======
 @section('content')
 <div class="search-containter">
     <h1>Stageplaatsen</h1>
     <p>{{$internships->count()}} Stageplaats(en)</p>
     @if ($internships->count() > 0)
->>>>>>> 7994d7e4daf7fda6d4ba6495f102d43381f321ca
 
-<<<<<<< HEAD
-@section('content')
-<h1>Home</h1>
-||||||| d10cfb6
-<body>
-    <h1>hoi</h1>
-</body>
-=======
     @foreach($internships as $internship)
     <div class="card-deck mb-3 text-center">
         <div class="card mb-4 shadow-sm">
@@ -42,17 +15,7 @@ Home
             </div>
             <div class="card-body">
                 <p>{{ $internship->bio }}</p>
->>>>>>> 7994d7e4daf7fda6d4ba6495f102d43381f321ca
 
-<<<<<<< HEAD
-<a class="btn btn-primary" href="/students" >Students</a>
-<a class="btn btn-primary" href="/internships" >Internships</a>
-<a class="btn btn-primary" href="/companies" >Companies</a>
-
-@endsection
-||||||| d10cfb6
-</html>
-=======
             </div>
 
         </div>
@@ -65,4 +28,3 @@ Home
 
 </div>
 @endsection
->>>>>>> 7994d7e4daf7fda6d4ba6495f102d43381f321ca

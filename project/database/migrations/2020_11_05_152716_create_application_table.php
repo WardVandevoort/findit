ff44dd0d7e1f5a09560b9cde9cf8e8609c3a4b1a
RaniVerelst <56079984+RaniVerelst@users.noreply.github.com>
2020-11-05 16:55:04 +0100

    added migration for applications

---
+++ project/database/migrations/2020_11_05_152716_create_application_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('internship_id');
+            $table->text('motivation');
+            $table->integer('status')->default('1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}

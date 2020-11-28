ef06b43ab9f26546d0908a12b149ee83199556cf
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-11-28 18:40:23 +0100

    First try of envoy tasks, still in testing

---
+++ project/Envoy.blade.php
@@ -0,0 +1,21 @@
+@servers(['production' => 'findit@172.105.73.78 -p22000', 'dev' => 'findit@172.105.73.78 -p22000'])
+
+@task('deploy-production', ['on' => 'production'])
+cd /home/findit/app/findit/project
+php artisan down
+git reset --hard HEAD
+git pull origin master
+php composer.phar dump-autoload
+php artisan migrate --force
+php artisan up
+@endtask
+
+@task('deploy-dev', ['on' => 'dev'])
+cd /home/findit/app/findit/project
+php artisan down
+git reset --hard HEAD
+git pull origin master
+php composer.phar dump-autoload
+php artisan migrate --force
+php artisan up
+@endtask

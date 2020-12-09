3956844ce4fa750aaf5b400cd4e7773bd29610ef
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-12-09 01:12:50 +0100

    Laravel Envoy works perfectly, if you get error on permission denied please search to ssh-agent and add your private key on it.

--- project/Envoy.blade.php
+++ project/Envoy.blade.php
@@ -1,13 +1,28 @@
 @servers(['production' => '-A deploybot@172.105.73.78', 'dev' => '-A deploybot@172.105.73.78'])
 
+@story('deploy')
+    deploy-production
+    deploy-dev
+@endstory
+
 @task('deploy-production', ['on' => 'production'])
 cd /home/deploybot/app/findit/project
 php artisan down
 git reset --hard HEAD
 git pull origin master
-php composer.phar dump-autoload
+composer dump-autoload -o
 php artisan migrate --force
 php artisan up
+@endtask
+
+@task('production-up', ['on' => 'production'])
+cd /home/deploybot/app/findit/project
+php artisan up
+@endtask
+
+@task('production-down', ['on' => 'production'])
+cd /home/deploybot/app/findit/project
+php artisan down
 @endtask
 
 @task('deploy-dev', ['on' => 'dev'])
@@ -15,7 +30,17 @@ cd /home/deploybot/beta-app/findit/project
 php artisan down
 git reset --hard HEAD
 git pull origin master
-php composer.phar dump-autoload
+composer dump-autoload -o
 php artisan migrate --force
 php artisan up
+@endtask
+
+@task('dev-up', ['on' => 'dev'])
+cd /home/deploybot/beta-app/findit/project
+php artisan up
+@endtask
+
+@task('dev-down', ['on' => 'dev'])
+cd /home/deploybot/beta-app/findit/project
+php artisan down
 @endtask
